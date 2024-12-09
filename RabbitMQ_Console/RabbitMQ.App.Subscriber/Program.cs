f05964fce3de8e8cedd450073428a95e8769d841
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2024-12-09 21:27:12 +0300

    Direct Exchange

--- RabbitMQ_Console/RabbitMQ.App.Subscriber/Program.cs
+++ RabbitMQ_Console/RabbitMQ.App.Subscriber/Program.cs
@@ -11,31 +11,30 @@ var factory = new ConnectionFactory
 
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
-//await channel.ExchangeDeclareAsync("logs-fanout", ExchangeType.Fanout, durable: true); write or do not doesn't matter 
+//await channel.ExchangeDeclareAsync("logs-fanout", ExchangeType.Direct, durable: true); write or do not doesn't matter 
 
 
-//var randomQueueName = "logs-database-save-queue";
-//await channel.QueueDeclareAsync(randomQueueName, true, false, false); permanent queue - will be deleted after program stops working. - created by consumer
-
-var queueResult = await channel.QueueDeclareAsync();//temporary queue - will be deleted after program stops working. - created by consumer
-var randomQueueName = queueResult.QueueName;
-
-await channel.QueueBindAsync(randomQueueName, "logs-fanout", "", null);
-
+var queueName = "direct-queue-Critical";
 await channel.BasicQosAsync(0, 1, false); // 0 : size could be anything , 1: once at a time send to subscriber , false: do not split messages evenly.
 //false wont require new instance of rmq, it pulls from cache
 
-Console.WriteLine("Logs Listening...");
+
 
 var consumer = new AsyncEventingBasicConsumer(channel);
-await channel.BasicConsumeAsync(queue: randomQueueName, autoAck: false, consumer: consumer); //autoAck : instantly delete / See the result first then delete
 
+await channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer: consumer);
+
+//autoAck : instantly delete / See the result first then delete
+Console.WriteLine("Logs Listening...");
+int messageCounter = 0;
 consumer.ReceivedAsync += async (object sender, BasicDeliverEventArgs @event) =>
 {
     var message = Encoding.UTF8.GetString(@event.Body.ToArray());
-    await Task.Delay(1000);
-    Console.WriteLine("Gelen Mesaj : " + message);
-    await  channel.BasicAckAsync(deliveryTag: @event.DeliveryTag, multiple: true);
+    //await Task.Delay(1000);
+    messageCounter++;
+    Console.WriteLine("Gelen Mesaj : " + message +" "+ messageCounter);
+    // File.AppendAllText("log-critical.txt", message+ "\n"); writing text file
+    await channel.BasicAckAsync(deliveryTag: @event.DeliveryTag, multiple: false);
 };
 
 Console.ReadLine();
--- RabbitMQ_Console/RabbitMQ.App/Program.cs
+++ RabbitMQ_Console/RabbitMQ.App/Program.cs
@@ -12,16 +12,41 @@ using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
 
-await channel.ExchangeDeclareAsync("logs-fanout", ExchangeType.Fanout, durable: true); //durable : true means exchange will not be deleted after running the program.
+//Direct Exchange Needs Route for specifying which queue will it bind to itself for filtering.
+await channel.ExchangeDeclareAsync("logs-direct", ExchangeType.Direct, durable: true); //durable : true means exchange will not be deleted after running the program.
+
+
+var logNames = Enum.GetNames(typeof(LogNames)).ToList();
+
+foreach (var logName in logNames)
+{
+    var routeKey = $"route-{logName}";
+    var queueName = $"direct-queue-{logName}";
+
+    await channel.QueueDeclareAsync(queueName, true, false, false);
+    await channel.QueueBindAsync(queueName, "logs-direct", routeKey, null);
+}
+
+
 
 
 foreach (var x in Enumerable.Range(1, 50))
 {
-    string message = $"log {x}";
+    LogNames log = (LogNames)new Random().Next(1, 5);
+    string message = $"log-type: {log}";
     var messageBody = Encoding.UTF8.GetBytes(message);
-    await channel.BasicPublishAsync(exchange: "logs-fanout", routingKey: "", body: messageBody);
+    var routeKey = $"route-{log}";
+    await channel.BasicPublishAsync(exchange: "logs-direct", routingKey: routeKey, body: messageBody);
 
-    Console.WriteLine($"Mesaj Gönderildi {message}");
+    Console.WriteLine($"Log gönderilmiştir : {message}");
 }
 
 Console.ReadLine();
+
+public enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
