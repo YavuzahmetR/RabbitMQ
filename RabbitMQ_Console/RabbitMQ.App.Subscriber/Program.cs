f5910f728f5f207b509e4290dad1dff74715c555
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2024-12-09 17:27:31 +0300

    Fundamental Work Publisher-Queue-Consumer-Subscriber

---
+++ RabbitMQ_Console/RabbitMQ.App.Subscriber/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://ooparaka:ax-HHpY3d-7GXX2237tka-5qQFhc8NEM@sparrow.rmq.cloudamqp.com/ooparaka")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+//await channel.QueueDeclareAsync("hello-queue", durable: true, exclusive: false, autoDelete: false); write or do not doesn't matter 
+
+
+
+
+await channel.BasicQosAsync(0, 1, false); // 0 : size could be anything , 1: once at a time send to subscriber , false: do not split messages evenly.
+//false wont require new instance of rmq, it pulls from cache
+
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+await channel.BasicConsumeAsync(queue: "hello-queue", autoAck: false, consumer: consumer); //autoAck : instantly delete / See the result first then delete
+
+consumer.ReceivedAsync += async (object sender, BasicDeliverEventArgs @event) =>
+{
+    var message = Encoding.UTF8.GetString(@event.Body.ToArray());
+    await Task.Delay(2000);
+    Console.WriteLine("Gelen Mesaj : " + message);
+   await  channel.BasicAckAsync(deliveryTag: @event.DeliveryTag, multiple: true);
+    //return Task.CompletedTask;
+};
+
+Console.ReadLine();
+
+
+//Console.WriteLine("Mesaj Gönderildi");
--- RabbitMQ_Console/RabbitMQ.App/Program.cs
+++ RabbitMQ_Console/RabbitMQ.App/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://ooparaka:ax-HHpY3d-7GXX2237tka-5qQFhc8NEM@sparrow.rmq.cloudamqp.com/ooparaka")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync(); 
+
+
+await channel.QueueDeclareAsync("hello-queue", durable: true, exclusive: false, autoDelete: false);
+
+Enumerable.Range(1, 50).ToList().ForEach(async x =>
+{
+    string message = "hello world";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+    await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello-queue", body: messageBody);
+
+    Console.WriteLine("Mesaj Gönderildi");
+});
+
+Console.ReadLine();
